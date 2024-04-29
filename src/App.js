724afa560129cf9dceed1fb1cb3e3a114939c5a9
Sanjayatennekoon <sanjayatennekoon4@gmail.com>
2024-04-29 16:38:50 +0530

    editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "react-quill/dist/quill.snow.css";
+
+import ImageResize from "quill-image-resize-module-react";
+import ReactQuill, { Quill } from "react-quill";
+
+<script src="/node_modules/quill-image-resize-module/image-resize.min.js"></script>;
 
 function App() {
+  const [value, setValue] = useState("");
+  const quillRef = useRef(null);
+  // Window.Quill = Quill;
+
+  Quill.register("modules/imageResize", ImageResize);
+
+  const toolbarOptions = [
+    ["bold", "italic", "underline", "strike"], // toggled buttons
+    ["blockquote", "code-block"],
+    ["link", "image", "formula"],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+    [{ script: "sub" }, { script: "super" }], // superscript/subscript
+    [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+    [{ direction: "rtl" }], // text direction
+
+    [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ["clean"],
+  ];
+  const modules = {
+    toolbar: {
+      container: toolbarOptions,
+      handlers: {
+        image: () => {
+          const url = prompt("Enter the URL of the image:");
+          if (url) {
+            const editor = quillRef.current.getEditor();
+            const range = editor.getSelection();
+            editor.insertEmbed(range.index, "image", url, "user");
+          }
+        },
+      },
+    },
+    imageResize: {
+      parchment: Quill.import("parchment"),
+      //modules: ["Resize", "DisplaySize"],
+    },
+  };
+
+  const data = (e) => {
+    //setValue(e);
+    console.log(e);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactQuill
+        modules={modules}
+        theme="snow"
+        value={value}
+        onChange={(e) => {
+          data(e);
+        }}
+        ref={quillRef}
+      />
+
+      <button
+        onClick={() => {
+          console.log("values are=");
+        }}
+      >
+        save
+      </button>
     </div>
   );
 }
